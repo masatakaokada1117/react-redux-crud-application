0d96e927c3e55243543436a455b5f4592be3b216
Masataka Okada <masataka.okada1117@gmail.com>
2020-05-24 23:39:08 +0900

    learn jsx syntax

--- react-redux-crud-application/src/App.js
+++ react-redux-crud-application/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function App() {
-  return React.createElement(
-    "h1",
-    null,
-    "Hello, World!!"
-  );
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => console.log("Hi!!")}/>
+    </React.Fragment>
+  )
 }
 
 export default App;
